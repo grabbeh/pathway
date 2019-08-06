6cac997e0d3e43ff62134bd138a9ecb2c27699ee
Michael <mbg@outlook.com>
2019-08-06 23:44:15 +0100

    Remove 404

--- src/components/CenterSection.js
+++ src/components/CenterSection.js
@@ -3,11 +3,13 @@ import Flex from './Flex'
 import Box from './Box'
 
 const CenterSection = ({ children }) => (
-  <Flex justifyContent='center'>
-    <Box px={[3, 0, 0]} py={5} maxWidth={800}>
-      {children}
-    </Box>
-  </Flex>
+  <section>
+    <Flex justifyContent='center'>
+      <Box px={[3, 0, 0]} py={5} maxWidth={800}>
+        {children}
+      </Box>
+    </Flex>
+  </section>
 )
 
 export default CenterSection
