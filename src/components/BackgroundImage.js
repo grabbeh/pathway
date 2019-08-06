519364cc4d7264f85a99356025818b1519fcbf20
Michael <mbg@outlook.com>
2019-08-06 15:24:42 +0100

    Update BackgroundImage.js

--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import { graphql, StaticQuery } from 'gatsby'
 import styled from 'styled-components'
-
 import BackgroundImage from 'gatsby-background-image'
 
 const BackgroundSection = ({ className }) => (
     <StaticQuery query={graphql`
       query {
-        desktop: file(relativePath: { eq: "seamless-bg-desktop.jpg" }) {
-          childImageSharp {
-            fluid(quality: 90, maxWidth: 4160) {
-              ...GatsbyImageSharpFluid_withWebp
+         allContentfulHomePage {
+      edges {
+        node {
+          headerImage {
+            fluid(maxWidth: 1200) {
+              ...GatsbyContentfulFluid
             }
           }
         }
       }
+    }
+      }
     `}
      render={data => {
        // Set ImageData.
-       const imageData = data.desktop.childImageSharp.fluid
+       const imageData = data.allContentfulHomePage.headerImage.fluid
        return (
           <BackgroundImage Tag="section"
                            className={className}
