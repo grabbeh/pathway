dca3008b9bcafcd96ce723e00ba3d28500555449
Michael <mbg@outlook.com>
2019-08-12 17:28:02 +0100

    Update Wrapper.js

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -68,7 +68,7 @@ const Wrapper = props => {
           <Box>
             <Header />
             <animated.div style={animProps}>
-              <Box>{props.children}</Box>
+              <Box zIndex={open ? -1 : 9999}>{props.children}</Box>
             </animated.div>
             <Footer />
           </Box>
