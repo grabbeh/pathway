79c4ff10f0fd06590c3e066ea2e7abb9827b428e
Michael <mbg@outlook.com>
2019-08-12 16:15:50 +0100

    Update Wrapper.js

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -41,7 +41,8 @@ const Wrapper = props => {
 
   const animProps = useSpring({
     config: { duration: 300 },
-    opacity: open ? 0 : 1
+    opacity: open ? 0 : 1,
+    visibility: 'hidden' : 'visible'
   })
 
   return (
