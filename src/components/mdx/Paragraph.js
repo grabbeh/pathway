44ff54eba80c57fa5be36b21080e6ec5ffd3096e
Michael <mbg@outlook.com>
2019-08-19 14:42:03 +0100

    Update Paragraph.js

--- src/components/mdx/Paragraph.js
+++ src/components/mdx/Paragraph.js
@@ -4,7 +4,7 @@ import Text from '../typography/Text'
 
 
 export const Paragraph = (props) => (
-  <Text.p fontSize={3} lineHeight='text'>{props.children}</Text.p>
+  <Text.p fontSize={3} fontWeight='text' lineHeight='text'>{props.children}</Text.p>
 )
 
 
