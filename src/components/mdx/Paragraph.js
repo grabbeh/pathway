a80e55f61b901ad82018a178c2c9a3777c99b4f1
Michael <mbg@outlook.com>
2019-08-19 14:45:24 +0100

    Update Paragraph.js

--- src/components/mdx/Paragraph.js
+++ src/components/mdx/Paragraph.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import Text from '../typography/Text'
 
-
-
 export const Paragraph = (props) => (
-  <Text.p fontSize={3} fontWeight='text' lineHeight='text'>{props.children}</Text.p>
+  <Text.p color='grey' fontSize={3} fontWeight='text' lineHeight='text'>{props.children}</Text.p>
 )
-
-
 
 export default Paragraph
