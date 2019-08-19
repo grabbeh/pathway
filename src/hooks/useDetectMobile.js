b9a99342603033f369fbcb328875d91879145af0
Michael <mbg@outlook.com>
2019-08-19 23:49:28 +0100

    Window

--- src/hooks/useDetectMobile.js
+++ src/hooks/useDetectMobile.js
@@ -1,21 +1,21 @@
 import { useState, useEffect } from 'react'
 
 function useDetectMobile () {
-  const [width, setWidth] = useState(window.innerWidth)
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth)
-    if (typeof window !== 'undefined') {
+  if (typeof window !== 'undefined') {
+    const [width, setWidth] = useState(window.innerWidth)
+
+    useEffect(() => {
+      const handleResize = () => setWidth(window.innerWidth)
       window.addEventListener('resize', handleResize)
-    }
-    if (typeof window !== 'undefined') {
       return () => {
         window.removeEventListener('resize', handleResize)
       }
-    }
-  }, [])
-  let mobile = false
-  if (width < 640) mobile = true
-  return mobile
+    }, [])
+
+    let mobile = false
+    if (width < 640) mobile = true
+    return mobile
+  }
 }
 
 export default useDetectMobile
