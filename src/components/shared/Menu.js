89aecb60b836ff4139fcf64a7b2abc61dcff8aa2
Michael <mbg@outlook.com>
2019-08-20 11:36:44 +0100

    Update Menu.js

--- src/components/shared/Menu.js
+++ src/components/shared/Menu.js
@@ -12,41 +12,43 @@ const Menu = ({ open, toggleOpen, navigationItems }) => {
   })
   return (
     <animated.div style={props}>
-      <Box
-        onClick={toggleOpen}
-        width='100%'
-        ml={open ? 0 : '100%'}
-        zIndex={open ? 9999 : -1}
-        position='fixed'
-        aria-hidden={!open}
-        top={65}
-        left={0}
-        bg='white'
-        pt={[5, 6]}
-      >
-        <Box>
-          <Flex height='100vh' justifyContent={['flex-start', 'center']}>
-            <Box ml={[3, 0]}>
-              {navigationItems.map(({ url, title }, i) => {
-                return (
-                  <Box mb={3} key={i}>
-                    <Link to={`/${url}`}>
-                      <Text
-                        textAlign={['left', 'center']}
-                        color='grey'
-                        fontWeight='bold'
-                        fontSize={[4, 5]}
-                      >
-                        {title}
-                      </Text>
-                    </Link>
-                  </Box>
-                )
-              })}
-            </Box>
-          </Flex>
+      <nav>
+        <Box
+          onClick={toggleOpen}
+          width='100%'
+          ml={open ? 0 : '100%'}
+          zIndex={open ? 9999 : -1}
+          position='fixed'
+          aria-hidden={!open}
+          top={65}
+          left={0}
+          bg='white'
+          pt={[5, 6]}
+        >
+          <Box>
+            <Flex height='100vh' justifyContent={['flex-start', 'center']}>
+              <Box ml={[3, 0]}>
+                {navigationItems.map(({ url, title }, i) => {
+                  return (
+                    <Box mb={3} key={i}>
+                      <Link to={`/${url}`}>
+                        <Text
+                          textAlign={['left', 'center']}
+                          color='grey'
+                          fontWeight='bold'
+                          fontSize={[4, 5]}
+                        >
+                          {title}
+                        </Text>
+                      </Link>
+                    </Box>
+                  )
+                })}
+              </Box>
+            </Flex>
+          </Box>
         </Box>
-      </Box>
+      </nav>
     </animated.div>
   )
 }
