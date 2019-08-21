0da857071b7bf11d96411a94ead1d83d8681f756
Michael <mbg@outlook.com>
2019-08-21 09:36:24 +0100

    Update Wrapper.js

--- src/components/shared/Wrapper.js
+++ src/components/shared/Wrapper.js
@@ -80,7 +80,7 @@ const Wrapper = props => {
             <Box>
               <Header />
               <animated.div style={animProps}>
-                <Box aria-hidden={open} zIndex={open ? -1 : 9999}>
+                <Box aria-hidden={open} hidden={open} zIndex={open ? -1 : 9999}>
                   {props.children}
                 </Box>
               </animated.div>
