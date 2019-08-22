d5eb8f12829e9787bfb207b19b654aa5a036e725
Michael <mbg@outlook.com>
2019-08-22 15:23:36 +0100

    Update Heading.js

--- src/components/general/Heading.js
+++ src/components/general/Heading.js
@@ -4,7 +4,7 @@ import Text from '../typography/Text'
 const H1 = Text.withComponent('h1')
 
 const Heading = ({ children, color = 'green' }) => (
-  <H1 fontSize={[7, 8]} lineHeight='68px' m={0} color={color} mb={3}>
+  <H1 fontSize={[6, 8]} lineHeight='68px' m={0} color={color} mb={3}>
     {children}
   </H1>
 )
