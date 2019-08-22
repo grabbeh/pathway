2a91186e21b1cce143737cbd2b68a254760a9cfc
Michael <mbg@outlook.com>
2019-08-23 00:11:00 +0100

    JSON table

--- src/components/what-we-do/PathwayTeam.js
+++ src/components/what-we-do/PathwayTeam.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import Section from '../general/StandardSection'
 import SectionTitle from '../typography/SectionTitle'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Animation from '../animations/ScrollAnimation'
+import styled from 'styled-components'
+import Flex from '../general/Flex'
+import Text from '../typography/Text'
+import Box from '../general/Box'
 
 const query = graphql`
   query {
     allContentfulWwdPage {
       edges {
         node {
-          pathwayTeamTitle
-          hospitalTeamTable {
-            childMdx {
-              body
+          hospitalTeamData {
+            rows {
+              text
+              values
             }
+            headers
           }
+          pathwayTeamTitle
         }
       }
     }
@@ -26,18 +32,82 @@ const PathwayTeam = () => {
   const data = useStaticQuery(query)
   let {
     pathwayTeamTitle,
-    hospitalTeamTable
+    hospitalTeamData
   } = data.allContentfulWwdPage.edges[0].node
+
+  const tableHeaders = (
+    <thead>
+      <Tr>
+        {hospitalTeamData.headers.map((header, i) => (
+          <Td key={i}>
+            <Text fontWeight='bold'>{header}</Text>
+          </Td>
+        ))}
+      </Tr>
+    </thead>
+  )
+
+  const tableBody = hospitalTeamData.rows.map((row, i) => {
+    return (
+      <Fragment key={i}>
+        <Tr>
+          <Td>
+            <Text textAlign='right' size='l'>
+              {row.text}
+            </Text>
+          </Td>
+          {row.values.map((column, i) => (
+            <Fragment key={i}>
+              {column ? (
+                <Td>
+                  <Flex justifyContent='center'>
+                    <Box borderRadius={5} height={10} width={10} bg='green' />
+                  </Flex>
+                </Td>
+              ) : (
+                <Td>
+                  <Text size='l' />
+                </Td>
+              )}
+            </Fragment>
+          ))}
+        </Tr>
+      </Fragment>
+    )
+  })
+
   return (
     <Section bg='lightGrey'>
       <Animation>
         <SectionTitle>{pathwayTeamTitle}</SectionTitle>
       </Animation>
-      <Animation>
-        <MDXRenderer>{hospitalTeamTable.childMdx.body}</MDXRenderer>
-      </Animation>
+      <Animation />
+      <Box my={3}>
+        <Table>
+          {tableHeaders}
+          <tbody>{tableBody}</tbody>
+        </Table>
+      </Box>
     </Section>
   )
 }
 
 export default PathwayTeam
+
+const Tr = styled('tr')`
+  border-bottom: 2px solid white;
+`
+
+const Table = styled('table')`
+  table-layout: fixed;
+  border-collapse: collapse;
+`
+/*
+const Th = styled('th')`
+padding: 10px;
+width: 100px;
+` */
+
+const Td = styled('td')`
+  padding: 5px;
+`
