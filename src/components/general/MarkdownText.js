c6df4c76fce207e83f71283c28b21eff35064e6f
Michael <mbg@outlook.com>
2019-08-22 15:46:35 +0100

    Update MarkdownText.js

--- src/components/general/MarkdownText.js
+++ src/components/general/MarkdownText.js
@@ -4,7 +4,7 @@ import Text from '../typography/Text'
 const MarkdownText = props => (
   <Text
     lineHeight='text'
-    fontSize={3}
+    fontSize={[1, 3]}
     {...props}
     dangerouslySetInnerHTML={{
       __html: props.html
