1ed6ca63521145bc1c3d31fb859a21ee9c19beb3
Michael <mbg@outlook.com>
2019-09-04 09:30:58 +0100

    Update ScrollAnimation.js

--- src/components/animations/ScrollAnimation.js
+++ src/components/animations/ScrollAnimation.js
@@ -19,8 +19,7 @@ export default ScrollAnimation
 
 const AnimatedBox = styled('div')`
   overflow: hidden;
-  transform: translateY(${props => (props.inView ? '0px' : '100px')})
-    scale(${props => (props.inView ? 1 : 0.5)});
+  transform: translateY(${props => (props.inView ? '0px' : '100px')});
   opacity: ${props => (props.inView ? 1 : 0)};
   transition: all 1s ease;
 `
