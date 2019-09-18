93bc7a795647e415af44b92f3188c5ad50d3fc0c
Michael <mbg@outlook.com>
2019-09-18 16:38:45 +0100

    Case study

--- src/components/general/WideSection.js
+++ src/components/general/WideSection.js
@@ -6,7 +6,7 @@ const WideSection = ({ id, children, bg }) => (
   <section id={id}>
     <Box bg={bg}>
       <Flex justifyContent='center'>
-        <Box mx={[3, 3, 6]} maxWidth={1200} py={5}>
+        <Box mx={3} maxWidth={1200} py={5}>
           {children}
         </Box>
       </Flex>
