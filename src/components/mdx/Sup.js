05a9de97f0b69197e805fc5366fe52ee5f586f72
Michael <mbg@outlook.com>
2019-09-18 14:41:34 +0100

    Sup

--- src/components/mdx/Sup.js
+++ src/components/mdx/Sup.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Text from '../typography/Text'
 
-export const Sup = props => {
-  console.log(props)
-  return (
-    <Text.sup {...props} fontSize={2} color='white'>
-      {props.children}
-    </Text.sup>
-  )
-}
+export const Sup = props => (
+  <Text.sup {...props} fontSize={2} color='white'>
+    {props.children}
+  </Text.sup>
+)
+
 export default Sup
