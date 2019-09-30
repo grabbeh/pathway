9e468cfe810db55881bfe5cb21d3d1fa8c06ac12
Michael <mbg@outlook.com>
2019-09-30 10:47:05 +0100

    Update components.js

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -5,7 +5,7 @@ import { Box } from '../components/general/index'
 export default {
   pre: props => (
     <Box
-      width={[1, 1 / 2]}
+      width={[1, 1, 3/4]}
       style={{ overflowX: 'auto' }}
       mt={3}
       bg='lightGrey'
