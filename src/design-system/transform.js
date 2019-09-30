b8bf6e8d2f4f3a9f20939c50990df90608e0b21b
Michael <mbg@outlook.com>
2019-09-30 12:02:04 +0100

    Update transform.js

--- src/design-system/transform.js
+++ src/design-system/transform.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components'
 
 export default styled('div')`
-  @media (min-width: 1024px) {
-    transform: scale(0.5);
-    transform-origin: top left;
-  }
+
 `
+
+// @media (min-width: 1024px) {
+   // transform: scale(0.5);
+   // transform-origin: top left;
+ // }
