4c413672910de03e9c8ddebbbe84743900eb9551
Michael <mbg@outlook.com>
2019-09-30 10:57:25 +0100

    Update transform.js

--- src/design-system/transform.js
+++ src/design-system/transform.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
 export default styled('div')`
-  transform: scale(0.5);
-  transform-origin: top left;
+  @media (max-width: 320px) {
+    transform: scale(0.5);
+    transform-origin: top left;
+  }
 `
