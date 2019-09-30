2f1567cba72e3a81086cf5709123bcd74c1626c3
Michael <mbg@outlook.com>
2019-09-30 16:08:36 +0100

    Update components.js

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import Prism from '@theme-ui/prism'
 import { Box } from '../components/general/index'
+import { Text } from '../components/typography'
 
 export default {
-  pre: props => props.children,
+   pre: props => (
+    <Box
+      width={[1, 1, 3/4]}
+      overflowX='auto'
+      mt={3}
+      p={2}
+    >
+     <Text fontSize={3}>
+      {props.children}
+     </Text>
+    </Box>
+  ),
   code: Prism
 }
