4564d4dbbce7c09e6fcf974a3931b9e7bedd039e
Michael <mbg@outlook.com>
2019-10-02 12:27:59 +0100

    Update IntroTitle.js

--- src/components/typography/IntroTitle.js
+++ src/components/typography/IntroTitle.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Text from './Text'
 
 const Intro = props => (
-  <Text.h3 {...props} fontSize={3} color={props.color}>
+  <Text.h3 fontWeight='body' {...props} fontSize={3} color={props.color}>
     {props.text}
   </Text.h3>
 )
