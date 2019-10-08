82aac644c772a0bbd6bbba924baf117a71aeacb7
Michael <mbg@outlook.com>
2019-10-08 23:43:02 +0100

    SEO

--- src/pages/pathway-support-package.js
+++ src/pages/pathway-support-package.js
@@ -1,16 +1,11 @@
 import React from 'react'
-import {
-  FindOutMore,
-  Footer,
-  seo as SEO,
-  Wrapper
-} from '../components/shared'
+import { FindOutMore, Footer, seo as SEO, Wrapper } from '../components/shared'
 import Hero from '../components/online-manual/Hero'
 import MainSection from '../components/online-manual/MainSection'
 
 const OnlineManual = () => (
   <Wrapper>
-    <SEO title='What you can do' />
+    <SEO title='Pathway support package' />
     <Hero />
     <MainSection />
     <FindOutMore pb={[4, 5]} bg='lightGrey' />
