d0a77fb7fde88bf8f30f762dc919b0b5f58f627f
Michael <mbg@outlook.com>
2019-10-14 17:38:36 +0100

    Update ULCHStats.js

--- src/components/what-we-do/ULCHStats.js
+++ src/components/what-we-do/ULCHStats.js
@@ -15,9 +15,7 @@ const query = graphql`
           ulchStats {
             figure
             text {
-              childMarkdownRemark {
-                html
-              }
+              text
             }
             subtitle
           }
